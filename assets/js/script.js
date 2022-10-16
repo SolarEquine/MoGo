aab19d4cf72f01f6e87fb6c8eaed621d0bf79ee6
Tia <nik12346@yandex.ru>
2022-10-16 15:48:00 +0300

    Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+"use strict"
 const burger = document.querySelector(".burger-menu");
 const menu = document.querySelector(".menu");
+const sliders = document.querySelectorAll(".reviews__slider");
+let direction =0;
 
 const wrapper = document.querySelector(".wrapper");
 
@@ -23,11 +26,52 @@ function toggleAccordion(target){
     target.parentNode.classList.add("active");
 }
 
+function moveSlider(target){
+    const review = target.parentNode.parentNode;
+    const slider = review.querySelector(".reviews__slider");
+    if (direction == 1){
+        slider.style.transform = "translateX(-100%)";
+        slider.style.justifyContent = "flex-start";
+
+        setTimeout(()=>{
+            slider.appendChild(slider.firstElementChild);
+            slider.style.transform = "translateX(0)";
+            
+            slider.style.transition = "none";
+            slider.style.transform = "translateX(0)";
+            setTimeout(()=>{
+              slider.style.transition = "all .2s";
+            }, 1);
+        }, 200);
+
+    }
+    else{
+        slider.prepend(slider.lastElementChild);
+        slider.style.transition="none";
+        slider.style.transform = "translateX(-100%)";
+
+        setTimeout(()=>{
+            slider.style.transition = "all .2s";
+            slider.style.transform = "translateX(0)"
+        }, 1)
+    }
+}
+
 wrapper.addEventListener("click", (event)=>{
     if (event.target.closest(".burger-menu") || event.target.closest(".menu__item")){
         toggleBurger();
     }
     if (event.target.closest(".accordion__header")){
         toggleAccordion(event.target);
+    }
+
+    if(event.target.closest(".reviews__btn_prev")){
+        direction = -1;
+        moveSlider(event.target);
+    }
+
+    if(event.target.closest(".reviews__btn_next")){
+        direction = 1;
+        moveSlider(event.target);
     }
 })
